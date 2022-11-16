17b891009a916616cea6e2d014f5b0269c70bdf5
Stavros Cheilitis <57620867+stavros98@users.noreply.github.com>
2022-11-16 21:26:41 +0200

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -8,7 +8,7 @@ $pass=$DB_PASS;
 
 
 if(gethostname()=='users.iee.ihu.gr') {
-	$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/staff/asidirop/mysql/run/mysql.sock');
+	$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/2016/it164778/mysql/run/mysql.sock');
 } else {
 		$pass=null;
         $mysqli = new mysqli($host, $user, $pass, $db);
